b1046dcec89d218bfef157de1716c5733d502961
leonardoyuan <leonardo.yuan1@gmail.com>
2025-07-19 19:13:11 -0300

    Primeiro Commit

---
+++ servico-de-reservas/src/main/java/com/leoyuan/servicoreservas/ServicoDeReservasApplication.java
@@ -0,0 +1,19 @@
+package com.leoyuan.servicoreservas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicoDeReservasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicoDeReservasApplication.class, args);
+		System.out.println("\n\n==============================================");
+		System.out.println("          SERVIÇO DE RESERVAS INICIADO         ");
+		System.out.println("==============================================\n\n");
+	}
+
+
+//commit teste
+
+}
---
+++ servico-de-reservas/src/test/java/com/leoyuan/servicoreservas/ServicoDeReservasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leoyuan.servicoreservas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicoDeReservasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
